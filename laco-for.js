0694b62f2fddb7adc7fe08e2a2b0500ecaec4e2c
felipe <bgame4495@gmail.com>
2026-03-05 16:37:20 -0300

    operadores_js

---
+++ laco-for.js
@@ -0,0 +1,9 @@
+// Laço de repetição "for" 
+
+for (let i = 0; i < 5; i++) {
+    console.log("Valor de i: " + i)
+}
+
+
+
+// for i in range(4):
---
+++ operadores.js
@@ -0,0 +1,18 @@
+// operadores logicos.
+
+let idade =10
+let peso = "10"
+
+console.log("= Comparação =")
+console.log("=COMPARANDO CONTEUDO")
+console.log(idade == peso)
+
+console.log("=COMPARANDO CONTEUDO E TIPO")
+console.log(idade === peso)
+
+console.log("= DIFERENTE =")
+console.log(idade != peso)  
+
+
+console.log("= DIFERENTE DE TIPO =")
+console.log(idade !== peso) 
