9ac06d0ed4ccda9439be6decf9188deebe040b7c
vladimir <vladimir.menezes@ba.estudante.senai.br>
2026-03-09 16:37:12 -0300

    salvar

---
+++ vetores-ex1.js
@@ -0,0 +1,26 @@
+// Exercicio
+
+// Crie um vetor com nome de seus familiares
+// Com 6 nomes. 
+
+
+// Use todos os comandos vistos em vetores apos criar a lista.
+
+const listadenomes = ["Daiane ,Roberto, Victor, Alana"]
+
+console.log("LISTA DE NOMES")
+console.log (listadenomes)
+
+console.log("\nExibindo apenas o primeiro elemtento")
+console.log(listadenomes[0])
+
+
+console.log("\nAdicionando elemento")
+listadenomes.push("Raissa")
+console.log(listadenomes)
+
+console.log("\nRemovendo um nome")
+listadenomes.splice(1,2)
+console.log(listadenomes)
+
+ 
